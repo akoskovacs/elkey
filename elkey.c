57e5c97ae09e2714f5f054eaf6103688ad84f740
Ákos Kovács <akoskovacs@gmx.com>
2014-07-06 19:35:43 +0200

    Use PCINT instead of INT0 and do everything once

--- elkey.c
+++ elkey.c
@@ -6,20 +6,20 @@
 #include <avr/interrupt.h>
 
 /* Null to disable power-down sleep mode */
-#define DO_PWR_DOWN 0
-#define DO_DELAY 1
+#define DO_PWR_DOWN 1
+#define DO_DELAY 0
 // For ATtiny13
 
 #define DELAY_MS 10
 
-/* Dit type, must be INT0  */
+/* Dit types */
 #define DOT_PORT     PORTB
 #define DOT_PIN      PINB
 #define DOT_DDR      DDRB
 #define DOT_MASK     _BV(PB1)
-#define DOT_INT      INT0
+#define DOT_INT      PCINT1
 
-/* Dah predefs, must be a PCINTn pin */
+/* Dah types */
 #define DASH_PORT    PORTB
 #define DASH_PIN     PINB
 #define DASH_DDR     DDRB
@@ -36,16 +36,11 @@
 #define OUT_OFF()    OUT_PORT &= ((uint8_t)~OUT_MASK)
 #define OUT_TOGGLE() OUT_PORT ^= ((uint8_t)OUT_MASK)
 
-#define IS_ALTERING(k) (k == DIT_ALTER || k == DAH_ALTER)
+//#define IS_ALTERING(k) (k == DIT_ALTER || k == DAH_ALTER)
+#define IS_DIT()       (DOT_PIN & DOT_MASK)
+#define IS_DAH()       (DASH_PIN & DASH_MASK)
+#define IS_ALTERING() (IS_DIT() && IS_DAH())
 
-enum MKEY_TYPE {
-    NONE = 0,
-    DIT,
-    DIT_ALTER, 
-    DAH,
-    DAH_ALTER 
-};
-#define ALTERING_OF(key) (key+1)
 
 //static const volatile WPM = 30;
 
@@ -57,11 +52,12 @@ void setup_pins(void)
     //DOT_PORT  |= DOT_MASK;
     //DASH_PORT |= DASH_MASK;
 
-    MCUCR     |= _BV(ISC00); /* Any logical change trigger interrupts */
+    //MCUCR     |= _BV(ISC00); /* Any logical change trigger interrupts */
 
-    /* Enable interrupts for INTn and PCINTn pins */
-    GIMSK     |= _BV(DOT_INT)|_BV(PCIE);
-    PCMSK     |= _BV(DASH_INT);
+//    GIMSK     |= _BV(DOT_INT)|_BV(PCIE);
+    /* Enable interrupts for PCINTn pins */
+    GIMSK     |= _BV(PCIE);
+    PCMSK     |= _BV(DOT_INT)|_BV(DASH_INT);
 }
 
 void setup_timer(void)
@@ -72,55 +68,25 @@ void setup_timer(void)
     OCR0A  = 100; // ~0.004 seconds (30 WPM)
 }
 
-static volatile enum MKEY_TYPE key_type = NONE;
-
-static void 
-set_key_type(const volatile uint8_t *pin, uint8_t pmask
-            , enum MKEY_TYPE this_key, enum MKEY_TYPE opp_key)
-{
-   if (((*pin) & pmask)) {
-       key_type = (key_type == opp_key) ? ALTERING_OF(opp_key) : this_key;
-   } else {
-       if (key_type == ALTERING_OF(this_key)) {
-           key_type = opp_key;
-       } else if (key_type == ALTERING_OF(opp_key)) {
-           key_type = opp_key;
-       } else {
-           key_type = NONE;
-       }
-   }
-}
-
-/* Dih interrupt */
-ISR(INT0_vect)
-{
-#if DO_DELAY
-   _delay_ms(DELAY_MS); 
-#endif 
-   set_key_type(&DOT_PIN, DOT_MASK, DIT, DAH);
-}
-
-static volatile uint8_t dah_counter   = 0;
 /*
- * Dah interrupt
- * Only one used -> no need for check the pin
+ * This interrupt handler mainly used to power-up the chip.
+ * It can also do debouncing, when DO_DELAY=1
  */
 ISR(PCINT0_vect)
 {
 #if DO_DELAY
    _delay_ms(DELAY_MS); 
 #endif 
-   /* If dah is unset (pressed, pulled down) */
-   set_key_type(&DASH_PIN, DASH_MASK, DAH, DIT);
-   dah_counter = 0;
 }
 
+enum MKEY_TYPE { NONE = 0, DIT, DAH };
+static volatile uint8_t     dah_counter = 0;
 static volatile enum MKEY_TYPE curr_key = NONE;
 static volatile bool           is_ready = false;
 
-void key_handler(enum MKEY_TYPE key)
+void key_handler()
 {
-    switch (key) {
+    switch (curr_key) {
         case DAH:
         if (dah_counter < 3) { // Standard morse timing
             is_ready = false;
@@ -147,31 +113,26 @@ void key_handler(enum MKEY_TYPE key)
 
 ISR(TIM0_COMPA_vect)
 {
-    enum MKEY_TYPE key = key_type;
-	if (key == NONE) {
-		return;
-	}
-	
-    /* No beeping currently, do the altering (if any) */
-    if (IS_ALTERING(key)) {
-		if (curr_key == NONE) {
-			if (key == DIT_ALTER) {
-				curr_key = DIT;
-			} else if (key == DAH_ALTER) {
-				curr_key = DAH;
-			}
-		}
-		if (is_ready) {
-			if (curr_key == DIT) {
-				curr_key = DAH;
-			} else if (curr_key == DAH) {
-				curr_key = DIT;
-			} 
-		}
-		key = curr_key;
+    if (is_ready) {
+        /* No beeping currently, do the altering (if any) */
+        if (IS_ALTERING()) {
+            if (curr_key == DIT) {
+                curr_key = DAH;
+            } else { 
+                curr_key = DIT;
+            }
+        } else {
+            if (IS_DIT()) {
+                curr_key = DIT;
+            } else if (IS_DAH()) {
+                curr_key = DAH;
+            } else {
+                curr_key = NONE;
+            }
+        }
     }
     /* Do the keying */
-    key_handler(key);
+    key_handler();
 }
 
 int main() /* __attribute__((noreturn)) */
@@ -186,7 +147,8 @@ int main() /* __attribute__((noreturn)) */
 #endif
 
     while (1) {
-        if (key_type == NONE) {
+        /* No key is pressed, go to bed... */
+        if (!IS_DIT() && !IS_DAH()) {
             OUT_OFF();
             dah_counter = 0;
             is_ready = true;
