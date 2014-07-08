5647f7e2e799473385efb2c934f8836fc6b542f0
Ákos Kovács <akoskovacs@gmx.com>
2014-07-08 23:31:18 +0200

    Added speed modification

--- elkey.c
+++ elkey.c
@@ -1,9 +1,9 @@
-//#define F_CPU   1E6
+//#define F_CPU   6e5
 #include <stdbool.h>
 #include <avr/io.h>
 #include <avr/sleep.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 /* Null to disable power-down sleep mode */
 #define DO_PWR_DOWN 1
@@ -36,11 +36,9 @@
 #define OUT_OFF()    OUT_PORT &= ((uint8_t)~OUT_MASK)
 #define OUT_TOGGLE() OUT_PORT ^= ((uint8_t)OUT_MASK)
 
-//#define IS_ALTERING(k) (k == DIT_ALTER || k == DAH_ALTER)
 #define IS_DIT()       (DOT_PIN & DOT_MASK)
 #define IS_DAH()       (DASH_PIN & DASH_MASK)
-#define IS_ALTERING() (IS_DIT() && IS_DAH())
-
+#define IS_ALTERING()  (IS_DIT() && IS_DAH())
 
 //static const volatile WPM = 30;
 
@@ -48,24 +46,33 @@ void setup_pins(void)
 {
     /* The output is the only output :) */
     OUT_DDR    = OUT_MASK;
-    /* Pull up for dih and dah */
-    //DOT_PORT  |= DOT_MASK;
-    //DASH_PORT |= DASH_MASK;
-
-    //MCUCR     |= _BV(ISC00); /* Any logical change trigger interrupts */
-
-//    GIMSK     |= _BV(DOT_INT)|_BV(PCIE);
-    /* Enable interrupts for PCINTn pins */
     GIMSK     |= _BV(PCIE);
     PCMSK     |= _BV(DOT_INT)|_BV(DASH_INT);
+}
+
+void enable_adc()
+{
+    /* Left adjust (use 8bit) & select channel 2 (ADC2) */
+    ADMUX |= _BV(ADLAR)|_BV(MUX1);
+    /* Enable the A/D converter with div128 */
+    ADCSRA |= _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1)|_BV(ADPS0);
+}
+
+
+uint8_t read_adc(void)
+{
+    ADMUX = 2; // Channel 2 (ADC2)
+    ADCSRA |= _BV(ADSC); // Start a single conversion
+    loop_until_bit_is_clear(ADCSRA, ADSC); // Wait til' done
+    return ADCH;
 }
 
 void setup_timer(void)
 {
     TCCR0A = _BV(WGM01);
-    TCCR0B = _BV(CS02)|_BV(CS00); // ClkIO/1024, CTC
+    TCCR0B = _BV(CS02)|_BV(CS00); // ClkIO/256, CTC @ 600Khz
     TIMSK0 = _BV(OCIE0A); // Enable timer match interrupt
-    OCR0A  = 100; // ~0.004 seconds (30 WPM)
+    OCR0A  = 40;
 }
 
 /*
@@ -77,12 +84,12 @@ ISR(PCINT0_vect)
 #if DO_DELAY
    _delay_ms(DELAY_MS); 
 #endif 
+   enable_adc();
 }
 
-enum MKEY_TYPE { NONE = 0, DIT, DAH };
-static volatile uint8_t     dah_counter = 0;
-static volatile enum MKEY_TYPE curr_key = NONE;
-static volatile bool           is_ready = false;
+static volatile enum MKEY_TYPE { NONE = 0, DIT, DAH } curr_key = NONE;
+static volatile uint8_t dah_counter   = 0;
+static volatile bool    is_ready      = false;
 
 void key_handler()
 {
@@ -113,6 +120,8 @@ void key_handler()
 
 ISR(TIM0_COMPA_vect)
 {
+    /* Update the code rate */
+    OCR0A = (uint8_t)read_adc()*40;
     if (is_ready) {
         /* No beeping currently, do the altering (if any) */
         if (IS_ALTERING()) {
@@ -139,6 +148,7 @@ int main() /* __attribute__((noreturn)) */
 {
     setup_pins();
     setup_timer();
+
     sei();
 
 #if DO_PWR_DOWN
@@ -152,8 +162,9 @@ int main() /* __attribute__((noreturn)) */
             OUT_OFF();
             dah_counter = 0;
             is_ready = true;
-			curr_key = NONE;
+            curr_key = NONE;
 #if DO_PWR_DOWN
+            ADCSRA &= (uint8_t)~_BV(ADEN); // disable adc
             sleep_cpu();
 #endif
         }
